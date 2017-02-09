85c785ee84d803a1233272d195121fe95455bfb5
cresta522 <cresta522@gmail.com>
2017-02-09 02:27:24 +0000

    Finish user destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
     # 変えたければ resources :users, param: :date
     @user = User.find(params[:id])
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+  
+  private
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
