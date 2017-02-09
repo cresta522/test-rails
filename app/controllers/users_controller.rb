ccf8c4aae28037fd4667403615dfd90c4cc79ed5
cresta522 <cresta522@gmail.com>
2017-02-09 02:20:59 +0000

    Finish user index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, :except=>[:show]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   def show
     # routesで設定している [resource]でのパラメータはデフォルトで :id
     # 変えたければ resources :users, param: :date
